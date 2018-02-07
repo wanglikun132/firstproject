7f60f3c6a4c335b072c25d9a6e79621823452d3b
user <738534839@qq.com>
2018-02-07 11:38:39 +0800

    shouci

---
+++ src/com/lanou/redis/Demo1.java
@@ -0,0 +1,15 @@
+package com.lanou.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class Demo1 {
+public static void main(String[] args) {
+	Jedis j = new  Jedis("127.0.0.1",6379);
+	j.auth("123456");
+	j.select(0);
+	j.set("1", "wanglikun");
+	
+	System.out.println(j.get("1"));
+	
+}
+}
